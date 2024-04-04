7e4341bca6bc651f68912ba9c2a0a854ff6ab926
Seungkyu Ahn <seungkyua@gmail.com>
2024-04-04 19:26:06 +0900

    kubernetes client-go

--- kubernetes/clientset.go
+++ kubernetes/clientset.go
@@ -20,31 +20,38 @@ func main() {
 	var namespace = "c09ajojmv"
 	secrets, err := adminClientSet.CoreV1().Secrets(namespace).Get(context.TODO(), namespace+"-tks-kubeconfig", metav1.GetOptions{})
 	if err != nil {
-		fmt.Printf("cannot found %s-tks-kubeconfig secret in %s namespace", namespace, namespace)
+		fmt.Printf("cannot found %s-tks-kubeconfig secret in %s namespace\n", namespace, namespace)
 	}
-	fmt.Printf("========================================\n")
+	fmt.Println("adminDynamicClientSet ========================================")
 	fmt.Printf("%#v\n", adminDynamicClientSet)
 	fmt.Printf("%+v\n", adminDynamicClientSet)
-	fmt.Printf("========================================\n")
+	fmt.Println("secrets.Data[\"value\"] ========================================")
 	fmt.Printf("%+v\n", string(secrets.Data["value"]))
 
 	// user cluster clientSet
 	config, err := clientcmd.RESTConfigFromKubeConfig(secrets.Data["value"])
 	if err != nil {
-		fmt.Printf("fail to build the k8s config from secret. Error - %s", err)
+		fmt.Printf("fail to build the k8s config from secret. Error - %s\n", err)
 	}
 
 	version := GetKubernetesVersion(config)
-	fmt.Printf("========================================\n")
+	fmt.Println("version ========================================")
 	fmt.Printf("Kubernetes version is %s\n", version)
 
 	clientSet, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		fmt.Printf("Fail to create the k8s client set. Errorf - %s", err)
 	}
-	fmt.Printf("========================================\n")
-	fmt.Printf("%+v", clientSet)
+	fmt.Println("clientSet ========================================")
+	fmt.Printf("%+v\n", clientSet)
 
+	// get cluster info
+	clusterInfo, err := clientSet.CoreV1().Services("kube-system").List(context.TODO(), metav1.ListOptions{LabelSelector: "kubernetes.io/cluster-service"})
+	if err != nil {
+		fmt.Printf("Failed to get cluster info: %v\n", err)
+	}
+	fmt.Printf("clusterInfo: ========================================\n")
+	fmt.Printf("%+v", clusterInfo.Items[0].ObjectMeta.Labels["kubernetes.io/cluster-service"])
 }
 
 func GetAdminClientSet() (*kubernetes.Clientset, *dynamic.DynamicClient) {
